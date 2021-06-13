f0a5552d42ca39c78e00a9f9391e824f70cfcc95
Milind-dev <milinddev1011@gmail.com>
2021-06-13 11:19:32 +0530

    first

---
+++ mplmenuOpen.js
@@ -0,0 +1,23 @@
+
+function openNavright() {
+    console.log("op")
+    document.getElementById("mplmySidepnel").style.width = "540px";
+    document.getElementById("mplmySidepnel").style.backgroundColor = "green";
+    document.getElementById("mplmySidepnel").style.opacity = "0.4";
+    document.getElementById("mplmySidepnel").style.Color = "black";
+    
+    
+    
+    
+}
+
+function closeNavright() {
+    console.log("o")   
+    document.getElementById("mplmySidepnel").style.width = "0";
+}
+
+
+
+function showpop(){
+    console.log("hello")
+}
